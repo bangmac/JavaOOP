2022798bcc1df048087738dd9483dbd59e34790a
bmv <leeloo1899@gmail.com>
2020-01-06 16:05:12 +0700

    done

---
+++ src/Triangle/Main.java
@@ -0,0 +1,13 @@
+package Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(1, 2, 3);
+        System.out.println(triangle.toString());
+
+        Triangle triangle1 = new Triangle(3, 4, 5);
+        System.out.println(triangle1.toString());
+        System.out.println("Area = " + triangle1.getArea());
+        System.out.println("Perimeter = " + triangle1.getPerimeter());
+    }
+}
